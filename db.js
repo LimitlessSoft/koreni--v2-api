f9b4d84b3268d1d31255f9e846f12bd93faf15d6
Aleksa Ristic <aleksa@termodom.rs>
2022-08-03 12:27:59 +0200

    DB Change

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql')
 
 const con = mysql.createConnection({
-    host: "koreni-v2-api.ct97tozelhun.eu-central-1.rds.amazonaws.com",
+    host: "174.138.184.42",
     port: "3306",
     user: "plivac555",
     password: "zRG69uq7nDdCqwSEURcJ",
-    database: "koreni-v2-main",
+    database: "koreni_v2",
     timezone: 'Z'
 })
 
