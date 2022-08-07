c9c1e7fb20e097991106691a1a652f0bb620b587
Aleksa Ristic <aleksar@limitlesssoft.com>
2022-08-07 23:02:04 +0200

    Multer increace file size

--- routers/upload.js
+++ routers/upload.js
@@ -20,7 +20,7 @@ const storage = multer.diskStorage({
     }
 })
 
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage, limits: { fileSize: 1048576 } })
 
 router.post('/', upload.single('thumbnail'), function(req, res) {
 
