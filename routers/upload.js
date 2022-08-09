05061147d62068d8f0cb1b71f12e78dea7e2ebf1
Aleksa Ristic <aleksar@limitlesssoft.com>
2022-08-09 20:34:05 +0200

    WebSlika Upload

--- routers/upload.js
+++ routers/upload.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const multer = require('multer')
 const path = require('path')
+const fs = require('fs')
+const web_slike_folder = './web_slike/'
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
@@ -20,7 +22,25 @@ const storage = multer.diskStorage({
     }
 })
 
+const storage_web_slike = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'web_slike/')
+    },
+    filename: function(req, file, cb)  {
+        var rs = function () {
+            return (Math.random() + 1).toString(36).substring(2)
+        }
+        var fn = ''
+        for(var i = 0; i < 10; i++) {
+            fn += rs()
+        }
+        var newFileName = fn + path.extname(file.originalname)
+        cb(null, newFileName)
+    }
+})
+
 const upload = multer({ storage: storage, limits: { fileSize: 1048576 } })
+const upload_web_slike = multer({ storage: storage_web_slike, limits: { fileSize: 1048576 }})
 
 router.post('/', upload.single('thumbnail'), function(req, res) {
 
@@ -31,4 +51,17 @@ router.post('/', upload.single('thumbnail'), function(req, res) {
     return res.status(400).send("none")
 })
 
+router.post('/web-slika', upload_web_slike.single('slika'), function(req, res) {
+    if(req.file != null) {
+        return res.status(200).send(req.file.filename)
+    }
+
+    return res.status(400).send('none')
+})
+
+router.get('/web-slika/list', (req, res) => {
+    fs.readdir(web_slike_folder, (err, files) => {
+        return res.json(files)
+    })
+})
 module.exports = router
